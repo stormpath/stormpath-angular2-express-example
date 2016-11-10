60c0eea7ae24f77598ca7061a5fb433628cdf9a9
angular-cli <angular-cli@angular.io>
2016-11-10 08:59:15 -0700

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Angular2ExpressExamplePage } from './app.po';
+
+describe('angular2-express-example App', function() {
+  let page: Angular2ExpressExamplePage;
+
+  beforeEach(() => {
+    page = new Angular2ExpressExamplePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
