243fdea2aba39da5920138bb172c3adfcd112242
Matt Raible <matt@raibledesigns.com>
2016-11-10 10:20:53 -0700

    Add express-stormpath and angular-stormpath to prove angular-stormpath works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Account, Stormpath } from 'angular-stormpath';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div *ngIf="(user$ | async)" class="row text-center">
+       <h2 class="">
+         Welcome, ({{ ( user$ | async ).fullName }}).
+       </h2>
+       <hr/>
+       <h4>What would you like to do?</h4>
+
+       <ul class="nav nav-pills nav-stacked text-centered">
+         <li role="presentation" (click)="logout()"><a href="#">Logout</a></li>
+       </ul>
+     </div>
+
+     <sp-authport></sp-authport>`,
+  providers: [Stormpath]
 })
 export class AppComponent {
-  title = 'app works!';
+  private user$: Observable<Account | boolean>;
+  private loggedIn$: Observable<boolean>;
+  private login: boolean;
+  private register: boolean;
+
+  constructor(public stormpath: Stormpath) {
+  }
+
+  ngOnInit() {
+    this.login = true;
+    this.register = false;
+    this.user$ = this.stormpath.user$;
+    this.loggedIn$ = this.user$.map(user => !!user);
+  }
+
+  showLogin() {
+    this.login = !(this.register = false);
+  }
+
+  showRegister() {
+    this.register = !(this.login = false);
+  }
+
+  logout() {
+    this.stormpath.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { StormpathModule } from 'angular-stormpath';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StormpathModule
   ],
   providers: [],
   bootstrap: [AppComponent]
